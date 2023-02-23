fa8e23e42e6188289a9e508da00d4964a23cab10
guruprince8 <guru.prince8@gmail.com>
2023-02-24 01:55:01 +0530

    main file chage

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import java.awt.desktop.SystemEventListener;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String name ="guru";
+        stepInto();
+        System.out.println("Hello world! "+name);
+
+    }
+    public static void stepInto(){
+        System.out.println("step into 1");
+        stepOver();
+        System.out.println("step into 2");
+        System.out.println("step into 3");
+    }
+
+    public static void stepOver(){
+        System.out.println("step into 1");
+        System.out.println("step into 2");
+        System.out.println("step into 3");
     }
 }
